0e30fa60f5f7f2511036889f338dce04cb935665
MenendezON <menendezon@gmail.com>
2024-02-07 20:22:06 +0000

    Update the model employee

--- lib/model/employee.dart
+++ lib/model/employee.dart
@@ -9,4 +9,15 @@ class Employee {
     this.lastName,
     this.status,
   });
+
+  factory Employee.fromMap(String id, Map<String, dynamic> data) {
+    return Employee(
+      employeeID: id,
+      firstName: data['firstName'],
+      lastName: data['lastName'],
+      avatar: data['avatar'],
+      email: data['email'],
+      status: data['status'],
+    );
+  }
 }
